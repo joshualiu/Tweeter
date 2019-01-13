ece72910b3ce11c81f2a51c80116572cae172ee0
zhonghaoliu <joshualiu94@gmail.com>
2019-01-13 13:27:39 -0500

    url

--- server/index.js
+++ server/index.js
@@ -21,8 +21,8 @@ app.use(
 // GET /css/main.css, it compiles ./stylesheets/main.scss
 
 const { MongoClient } = require("mongodb");
-// const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter";
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter";
+// const MONGODB_URI = process.env.MONGODB_URI;
 
 // connect to mongoDB:
 
